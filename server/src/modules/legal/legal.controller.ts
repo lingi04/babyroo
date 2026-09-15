d85c4c8d1cc8ffcc121f447d4022cc9101e5b185
janghohan <hanjh@gmail.com>
2026-09-15 11:18:44 +0900

    Add account deletion instructions page

--- server/src/modules/legal/legal.controller.ts
+++ server/src/modules/legal/legal.controller.ts
@@ -9,19 +9,36 @@ import { createReadStream, existsSync } from 'node:fs';
 import { join } from 'node:path';
 
 const PRIVACY_POLICY_FILENAME = 'privacy-policy.html';
+const ACCOUNT_DELETION_FILENAME = 'account-deletion.html';
 
 @Controller()
 export class LegalController {
   @Get('privacy')
   @Header('Content-Type', 'text/html; charset=utf-8')
   getPrivacyPolicy() {
-    const privacyPolicyPath = resolveStaticFile(PRIVACY_POLICY_FILENAME);
+    return this.streamStaticFile(
+      PRIVACY_POLICY_FILENAME,
+      'Privacy policy file was not found',
+    );
+  }
 
-    if (!privacyPolicyPath) {
-      throw new NotFoundException('Privacy policy file was not found');
+  @Get('account-deletion')
+  @Header('Content-Type', 'text/html; charset=utf-8')
+  getAccountDeletion() {
+    return this.streamStaticFile(
+      ACCOUNT_DELETION_FILENAME,
+      'Account deletion file was not found',
+    );
+  }
+
+  private streamStaticFile(filename: string, notFoundMessage: string) {
+    const staticFilePath = resolveStaticFile(filename);
+
+    if (!staticFilePath) {
+      throw new NotFoundException(notFoundMessage);
     }
 
-    return new StreamableFile(createReadStream(privacyPolicyPath));
+    return new StreamableFile(createReadStream(staticFilePath));
   }
 }
 
